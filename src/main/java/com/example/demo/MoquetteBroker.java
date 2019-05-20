0d2a39363f7f52cabf1a90e6ecd5e25a3ca816d4
Erik Levi <levi.erik@gmail.com>
2019-05-20 16:00:51 +0200

    aggiunti metodi per avviare e fermare il broker

--- src/main/java/com/example/demo/MoquetteBroker.java
+++ src/main/java/com/example/demo/MoquetteBroker.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import io.moquette.broker.Server;
 import io.moquette.broker.config.MemoryConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -11,11 +13,12 @@ import io.moquette.BrokerConstants;
 
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.util.Properties;
 
 @Component
 public class MoquetteBroker implements DisposableBean, InitializingBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(MoquetteBroker.class);
 
     /*
     @Value("${mqtt.serverport:1883}")
@@ -30,19 +33,31 @@ public class MoquetteBroker implements DisposableBean, InitializingBean {
 
     private Server server;
     private MemoryConfig config;
+    private boolean started = false;
 
     public void start() throws IOException {
-
+        if(started){
+            logger.info("server already started!");
+            return;
+        }
         config = new MemoryConfig(new Properties());
         config.setProperty(BrokerConstants.ALLOW_ANONYMOUS_PROPERTY_NAME, "false");
         config.setProperty(BrokerConstants.AUTHENTICATOR_CLASS_NAME, AuthenticationWrapper.class.getName());
         server = new Server();
         server.startServer(config);
+        started = true;
+    }
+
+    public void stopServer(){
+        server.stopServer();
+        started = false;
+        logger.info("Broker Stopped");
     }
 
     @Override
     public void destroy() throws Exception {
         server.stopServer();
+        started = false;
         System.out.println("Server stopped!");
     }
 
--- src/main/java/com/example/demo/MoquetteController.java
+++ src/main/java/com/example/demo/MoquetteController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +26,20 @@ public class MoquetteController {
                  .listConnectedClients()
                  .stream()
                  .map(ClientDescriptor::getClientID).collect(Collectors.toList());
+
     }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @RequestMapping(value = "/start", method = RequestMethod.GET)
+    public void start() throws IOException {
+        mb.start();
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @RequestMapping(value = "/stop", method = RequestMethod.GET)
+    public void stop(){
+        mb.stopServer();
+    }
+
 
 }
